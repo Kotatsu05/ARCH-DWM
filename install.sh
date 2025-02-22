e0a1690dd706caae8de47ba56698ab95cc56066a
Kotatsu <kotatsu.0550@gmail.com>
2025-02-22 11:21:27 +0100

    install

--- install.sh
+++ install.sh
@@ -5,9 +5,32 @@ sudo pacman -S fzf
 sudo pacman -S zsh
 sudo pacman -S neovim
 sudo pacman -S ttf-firacode-nerd
+sudo pacman -S brightnessctl
+sudo pacman -S xclip
+sudo pacman -S gcc
+sudo pacman -S makefile
+sudo pacman -S clangd
 
-sudo cp themes/Tokyonight-Dark /usr/share/themes
-sudo cp icons/dracula-icons-main /usr/share/icons
+sudo pacman -S libx11
+sudo pacman -S libxft
+sudo pacman -S libxinerama
+
+mkdir /usr/share/themes
+sudo cp -r themes/Tokyonight-Dark /usr/share/themes
+
+mkdir /usr/share/icons
+sudo cp -r icons/dracula-icons-main /usr/share/icons
+
+mkdir /etc/X11/xorg.conf.d/
 sudo cp etc/X11/xorg.conf.d/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 
-cp gtk-3.0 ~/.config/gtk-3.0
+mkdir ~/.config
+
+mkdir ~/.config/gtk-3.0
+cp -r gtk-3.0 ~/.config/gtk-3.0
+
+mkdir ~/.config/picom
+cp -r picom ~/.config/picom
+
+cd "$(suckless/dwm "$0")"
+sudo make clean install
